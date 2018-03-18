5671fb5ec78485634bb07be84f5fbe4d8a439c5c
Kaneko0107 <happiness.point@gmail.com>
2018-03-18 15:53:42 +0900

    ホーム画面でセッション情報を保持できるように

--- src/com/internousdev/alatanapizza/action/HomeAction.java
+++ src/com/internousdev/alatanapizza/action/HomeAction.java
@@ -2,25 +2,27 @@ package com.internousdev.alatanapizza.action;
 
 import java.util.Map;
 
+import org.apache.struts2.interceptor.SessionAware;
+
 import com.opensymphony.xwork2.ActionSupport;
 
-public class HomeAction extends ActionSupport {
+public class HomeAction extends ActionSupport implements SessionAware {
 
-	private Map<String,Object>session;
+	private Map<String,Object> session;
 
 	public String execute(){
 
-		if(!"saveId".equals("")){
-			session.get("saveId");
-		}
+//		if(!"saveId".equals("")){
+//			session.get("saveId");
+//		}
 		return SUCCESS;
 	}
 
 	public Map<String,Object> getSession(){
 		return session;
 	}
-	public void setSession(Map<String,Object> session){
-		this.session=session;
-	}
 
+	public void setSession(Map<String,Object> session){
+		this.session = session;
+	}
 }

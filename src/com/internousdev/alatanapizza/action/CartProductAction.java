30751a86176a79b381a699b01515e23e176963fa
Kaneko0107 <happiness.point@gmail.com>
2018-04-05 11:23:44 +0900

    CartActionに説明用のコメントを追加しました

--- src/com/internousdev/alatanapizza/action/CartProductAction.java
+++ src/com/internousdev/alatanapizza/action/CartProductAction.java
@@ -14,15 +14,12 @@ import com.opensymphony.xwork2.ActionSupport;
 
 public class CartProductAction extends ActionSupport implements SessionAware{
 
-	//商品ID_商品購入ページから持ってくる
-	private int productId;
+	private int productId;//商品ID_商品購入ページから持ってくる
 	private int product; // ピザ追加時のサイズ別の値段
 	private String errorMessage;
 
 	//セッション情報
 	private Map<String,Object>session;
-
-
 
 	//カート内の商品情報リスト
 	private ArrayList<CartInfoDTO>cartList = new ArrayList<CartInfoDTO>();
@@ -34,7 +31,8 @@ public class CartProductAction extends ActionSupport implements SessionAware{
 	private int count;
 	private CartInfoDAO dao = new CartInfoDAO();
 
-	//カート内の金額
+	//totalPriceは2種類の意味を持っている
+	//カート全体の金額を入れる場合と、個別の商品金額を入れる場合
 	public int totalPrice = 0;
 
 	//トッピング情報
@@ -68,20 +66,32 @@ public class CartProductAction extends ActionSupport implements SessionAware{
 		}
 
 		// ブラウザのリロード時はカートを取得してsuccessに入れる
+	    //もし商品IDまたは商品個数が0だった場合
 		if (productId == 0 || productCount == 0) {
+			//userIdのカートリストを取得する
 			cartList = dao.showUserCartList(userId);
+			//カートリストを渡して、カート全体の合計金額を計算する
 			totalPrice = calcTotalPrice(cartList);
 			return "success";
 		}
+
+		//getProductDetailsInfoがStringで値を取るようになっている為、productIdをString型にしている
+		//DAOからproductId（商品情報）を取得し、DTOのdetailという変数に代入している
 		ProductDTO detail = productDetailsDAO.getProductDetailsInfo(Integer.valueOf(productId).toString());
+		//もし、商品個数がマイナスだったら
 		if(productCount < 0){
+			//userIdのカートリストを取得する
 			cartList = dao.showUserCartList(userId);
+			//カートリストを渡して、カート全体の合計金額を計算する
 			totalPrice = calcTotalPrice(cartList);
+			//エラーメッセージを表示する
 			errorMessage = "購入個数が不正です。";
+			//successを返して、74行目と同じjspを表示させる
 			return "success";
 		}
 
 		//カートにトッピング追加時のための処理
+		//もし、トッピングidがnullではない場合、add(追加)する
 		if (topping_id_1 != null) toppings.add(1);
 		if (topping_id_2 != null) toppings.add(2);
 		if (topping_id_3 != null) toppings.add(3);
@@ -95,36 +105,61 @@ public class CartProductAction extends ActionSupport implements SessionAware{
 		if (topping_id_11 != null) toppings.add(11);
 		if (topping_id_12 != null) toppings.add(12);
 
-
+		//ピザをカートに入れる時に、MサイズかLサイズか判別するコード
+		//ピザサイズを未設定にする(nullを代入する）
 		String pizzaSize = null;
+		//product（追加した商品価格）が0ではない場合
 		if (product != 0) {
+			//追加した商品価格がMサイズの価格ならばピザサイズをMにする
 			if (detail.getMsize_price() == product) {
 				pizzaSize = "M";
+			//追加した商品価格がLサイズの価格ならばピザサイズをLにする
 			} else if (detail.getLsize_price() == product) {
 				pizzaSize = "L";
 			}
 		}
 
 		//カートが重複しているか確認する
+		//カートに追加ボタンを押して同じ商品だった場合、数を増やす機能
+
+		//CartInfoDAOのisAlreadyIntoCartで商品情報が重複しているか確認する
+		//確認した結果をcartIdに代入している
 		Integer cartId = dao.isAlreadyIntoCart(userId, productId, pizzaSize, toppings);
+
+		//cartIdがnullではない場合（商品情報が重複している場合）
 		if(cartId != null){
+			//CartInfoDAOのchangeProductStockで重複している商品の個数を変更する
+			//cartId(重複している商品のIdが入っている）
+			//productCount(カートに入れた個数が入っている）
 			count = dao.changeProductStock(productCount,cartId);
+
+		//商品情報が重複していない場合
 		}else{
+			//totalPrice（商品別の金額/ピザの場合はトッピング込みの金額）
+			//商品が重複していない為、全ての情報をputProductIntoCartに渡して、カート情報をつくっている
+			//作成したカート情報をcountに代入している
 			count = dao.putProductIntoCart(userId,productId,productCount,totalPrice, pizzaSize, toppings);
 		}
+
+		//userIdのカートリストを取得する
 		cartList = dao.showUserCartList(userId);
 
-
+		//カートリストを渡して、カート全体の合計金額を計算する
 		totalPrice = calcTotalPrice(cartList);
 		return SUCCESS;
 	}
 
-	//合計金額計算
-	public int calcTotalPrice(ArrayList<CartInfoDTO>cartList){
-		int totalPrice = 0;
-		for(CartInfoDTO dto : cartList){
-			totalPrice += dto.getPrice();
+	//合計金額計算を計算するメソッドの定義
+	//calcTotalPriceの処理内容
 
+	//カートリストが渡される
+	public int calcTotalPrice(ArrayList<CartInfoDTO>cartList){
+		//合計金額を初期化する
+		int totalPrice = 0;
+		//cartList内の個別のカート情報（dto)をfor文でまわしている
+		for(CartInfoDTO dto : cartList){
+			//totalPriceにgetした金額を足し合わせている
+			totalPrice += dto.getPrice();
 		}
 		return totalPrice;
 	}
